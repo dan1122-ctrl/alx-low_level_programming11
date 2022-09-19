3f23ba28bfc4a3cfc6fee21460bc5798de270096
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-19 06:21:31 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,10 @@
 
 /**
  *
- * *reset_to_98 - resets the value of given integer to 98
- *
- * *@n: parameter
- *
- * *Return : nothing
- *
- * */
+ * reset_to_98 - resets the value of given integer to 98
+ * @n: parameter
+ * Return : nothing
+ */
 
 
 
