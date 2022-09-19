34e1c1d719e8a1706d40b231fca21eeaae23b6cd
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-19 08:47:48 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,10 +3,8 @@
 #include "main.h"
 
 
-
 /**
- 
- * print_array - prints n element of array
+ * print_array - prints n element of array 
  * @a: array
  * @n: number of elements
  * Return: nothing
