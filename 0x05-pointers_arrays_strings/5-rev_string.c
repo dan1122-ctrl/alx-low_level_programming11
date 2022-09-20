f53564cfc39a502a6b341143b606c735c4db3c74
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-20 01:44:39 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,16 +12,16 @@ void rev_string(char *s);
 	   int i,c,k;
 	   char *a, aux;
 
-	   a=s;
+	   a = s;
 	   while (s[c] !='\0')
-	{
+       	{
 		c++;
 	}
 	  for (k=1; k < c; k++)
 	  {
 		  a++;
 	  }	  
-         for  (i=0; i<(c/2); i++)
+         for  (i=0; i < (c/2); i++)
 	  {
 		  aux = s[i];
 		  s[i] = *a;
