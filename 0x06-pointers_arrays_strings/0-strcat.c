10524378ab28355ae91ddbe516641438dd6a9228
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-22 05:04:48 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,35 @@
 #include "main.h"   
 #include<string.h>
 
-/**
-                   
+/** 
  * strcat - Concatenates the string pointed to by @src, including the terminat 
  * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
+ *
  * Return: A pointer to the destination string @dest.
  */
 char *strcat(char *dest, const char *src)
 {
-    char dest[]="daniel";
-    char src[10]="Teklemariam";
-    strcat(dest,src);
-    puts(dest);
+
+
+
+		int index = 0, dest_len = 0;
+
+
+
+			while (dest[index++])
+
+						dest_len++;
+
+
+
+				for (index = 0; src[index]; index++)
+
+							dest[dest_len++] = src[index];
+
+
+
+					return (dest);
 
 }
