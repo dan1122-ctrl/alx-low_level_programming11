93bcf6fec4300d357834688e1536ae8f694ba313
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-22 06:22:31 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,38 +15,27 @@ void print_line(char *c, int s, int l)
 
 		int j, k;
 
+		for (j = 0; j <= 9; j++)
 
+		{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
 
-			for (j = 0; j <= 9; j++)
+		else
+			printf("  ");
+		if (j % 2)
 
-					{
+			putchar(' ');
+ 
+		}
 
-								if (j <= s)
+		for (k = 0; k <= s; k++)
+		{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
 
-												printf("%02x", c[l * 10 + j]);
-
-										else
-
-														printf("  ");
-
-												if (j % 2)
-
-																putchar(' ');
-
-													}
-
-				for (k = 0; k <= s; k++)
-
-						{
-
-									if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-
-													putchar(c[l * 10 + k]);
-
-											else
-
-															putchar('.');
-
-												}
+		else
+			putchar('.');
+		}
 
 }
