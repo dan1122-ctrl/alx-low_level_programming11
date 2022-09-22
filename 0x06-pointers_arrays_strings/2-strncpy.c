bfa6b032793123e914886eac1562d3f307e648bd
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-22 12:20:03 -0700

    strcat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 /**
- * _strncpy - C function that copies a string, including the
+ * _strncpy -C function that copies a string, including the
  * terminating null byte, using at most an inputted number of bytes.
  * If the length of the source string is less than the maximum byte number,
  * the remainder of the destination string is filled with null bytes.
@@ -16,24 +17,17 @@
 
 
 char *_strncpy(char *dest, char *src, int n)
+
 {
+	int i; 
 
-		int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
 
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+	
+		dest[i] = '\0';
 
-
-			for (i = 0; i < n && src[i] != '\0'; i++)
-
-						dest[i] = src[i];
-
-
-
-				for ( ; i < n; i++)
-
-							dest[i] = '\0';
-
-
-
-					return (dest);
+      	return (dest);
 
 }
