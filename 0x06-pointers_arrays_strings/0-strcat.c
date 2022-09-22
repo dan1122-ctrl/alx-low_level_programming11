999c3c3939f4a487f6c90ba7fdfe91ab20208566
dan1122-ctrl <danieltekle88@gmail.com>
2022-09-22 03:21:50 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,9 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
-	while (dest[index++])
-		dest_len++
-			for (index = 0; src[index]; index++)
-				dest[dest_len++] = src[index];
-	return (dest);
+    char dest[]="daniel";
+    char src[10]="Teklemariam";
+    strcat(dest,src);
+    puts(dest);
+
 }
